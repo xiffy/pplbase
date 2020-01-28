f6d7cb1b9cfbb966470fff6e560e5194883262fa
Appie verschoor <A.Verschoor@softwareprofs.nl>
2020-01-28 16:48:21 +0100

    Testscript op linux, windowsgebruikers; good luck ;-)

--- alltests.sh
+++ alltests.sh
@@ -4,13 +4,14 @@
 # Dat levert isolatie van de testomgeving op en de vrijheid om fake data in te schieten.
 # Start the container
 
-docker run -p 9204:9200 -p 9304:9300 -de "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.5.2
+docker run -p 9204:9200 -p 9304:9300 -de "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.5.1
 
 # zet de omgevingsvariabelen op deze elasticsearch-docker
 export ELASTIC_HOSTS=localhost:9204
 export PYTHONPATH=$PWD:$PYTHONPATH
-sleep 10
-curl
+# wacht totdat de standalone elasticsearch is wakker (ik gok op 15 seconden op dit moment)
+sleep 15
+elasticdump --output=http://localhost:9204/softwareprofs --input=./test/data/softwareprofs_data.json --type=data
 python3 test/test_pplbase.py
 
 
